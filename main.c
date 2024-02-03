eb38120df8d3f7169fb0b8fe8ffd90989d894674
100101111011111110100 <arturzamaletdinov@gmail.com>
2024-02-03 09:42:44 +0300

    2

--- main.c
+++ main.c
@@ -4,13 +4,18 @@
 #define X 25
 #define Y 80
 int ** gen_arr(int x,int y);
+int life( int **b);
+void output(int **array);
+
 
 int main(){
     int **wisual_matrix=NULL;
     int **invis_matrix=NULL;
     if((wisual_matrix=gen_arr(X,Y))!=NULL&&(invis_matrix=gen_arr(X,Y))!=NULL){
-        printf("%p\n",wisual_matrix);
         printf("%p\n",invis_matrix);
+        life( invis_matrix);
+        //printf("%p\n",wisual_matrix);
+        
     }
     
     return 0;
@@ -22,29 +27,35 @@ int main(){
 int ** gen_arr(int x,int y){
     int ** a_ptr=(int **)malloc(sizeof(int *)*x+sizeof(int)*x*y);
     if(a_ptr!=NULL){
-        int *a_p=NULL;
-        for (size_t i = 0; i < x; i++){
-            a_p=(int*)a_ptr + sizeof(int*)*x + i*y*sizeof(int);
+        for (int i = 0; i < x; i++){
+            a_ptr[i]=(int*)(a_ptr + sizeof(int*)*x + i*y*sizeof(int));
+            printf("!%d %p!\n",i,a_ptr[i]);
         }
-        
     }
     return a_ptr;
 }
-
-int life(int **a, int **b){
-    while(1){
+// int **a,
+int life( int **b){
+    int check_cord(int **array,int c_x,int c_y);
+    int i=1;
+    
+    while(i){
         for (int x = 0; x < X; x++){
             for (int y = 0; y < Y; y++){
-                b[x][y];
+                printf("!%d %p!\n",x,b[x]);
+                // b[x][y]='0';
+                // printf("[%d][%d]==%d\n",x,y,b[x][y]);
             }
         }
+        output(b);
+        i--;
     }
     return 0;
 }
 
-void copy(int *a,int *b){
+// void copy(int *a,int *b){
 
-}
+// }
 
 int check_cord(int **array,int c_x,int c_y){
     int flag =0,n=0,n_x,n_y;
@@ -70,4 +81,14 @@ int check_cord(int **array,int c_x,int c_y){
         }
     }
     return flag;
+}
+
+
+void output(int **array){
+    for(int x=0;x<X;x++){
+        for(int y=0;y<Y;y++){
+            printf("%d",array[x][y]);
+        }
+        putchar('\n');
+    }
 }
